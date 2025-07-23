8ff42aa9157f19629063e1afa19bf1a0d34f1f4c
henokworkuliyew <hw93gojj@gmail.com>
2025-07-23 16:53:01 +0300

    feat: Implement Redux Saga for song CRUD operations with error handling

--- src/store/songs/songsSaga.js
+++ src/store/songs/songsSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery, all } from 'redux-saga/effects';
 import axios from 'axios';
 import {
   fetchSongsStart,
@@ -9,17 +9,19 @@ import {
   deleteSongSuccess,
 } from './songsSlice';
 
-function* fetchSongsSaga(action) {
+function* fetchSongs(action) {
   try {
-    yield put(fetchSongsStart());
-    const response = yield call(axios.get, `${process.env.API_BASE_URL}/songs?page=${action.payload}&limit=10`);
+    const page = action.payload || 1;
+    const response = yield call(axios.get, `${process.env.API_BASE_URL}/songs`, {
+      params: { page, limit: 10 },
+    });
     yield put(fetchSongsSuccess(response.data));
   } catch (error) {
     yield put(fetchSongsFailure(error.message));
   }
 }
 
-function* addSongSaga(action) {
+function* addSong(action) {
   try {
     const response = yield call(axios.post, `${process.env.API_BASE_URL}/songs`, action.payload);
     yield put(addSongSuccess(response.data));
@@ -28,16 +30,17 @@ function* addSongSaga(action) {
   }
 }
 
-function* updateSongSaga(action) {
+function* updateSong(action) {
   try {
-    const response = yield call(axios.put, `${process.env.API_BASE_URL}/songs/${action.payload.id}`, action.payload);
+    const { id, ...data } = action.payload;
+    const response = yield call(axios.put, `${process.env.API_BASE_URL}/songs/${id}`, data);
     yield put(updateSongSuccess(response.data));
   } catch (error) {
     yield put(fetchSongsFailure(error.message));
   }
 }
 
-function* deleteSongSaga(action) {
+function* deleteSong(action) {
   try {
     yield call(axios.delete, `${process.env.API_BASE_URL}/songs/${action.payload}`);
     yield put(deleteSongSuccess(action.payload));
@@ -46,9 +49,13 @@ function* deleteSongSaga(action) {
   }
 }
 
-export function* watchSongsSaga() {
-  yield takeEvery('songs/fetchSongs', fetchSongsSaga);
-  yield takeEvery('songs/addSong', addSongSaga);
-  yield takeEvery('songs/updateSong', updateSongSaga);
-  yield takeEvery('songs/deleteSong', deleteSongSaga);
+function* watchSongsSaga() {
+  yield all([
+    takeEvery('songs/fetchSongsStart', fetchSongs),
+    takeEvery('songs/addSong', addSong),
+    takeEvery('songs/updateSong', updateSong),
+    takeEvery('songs/deleteSong', deleteSong),
+  ]);
 }
+
+export { watchSongsSaga };
